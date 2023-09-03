886eed7a121a20cc3bd630c36dab341a3f5f85c6
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-03 03:29:34 -0700

    Move head to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Japanese Reader</title>
+        <meta
+          name="description"
+          content="A mobile-focused Japanese reader web app built with React, TypeScript, Next.js, TailwindCSS, and Zustand. Initialized with create-t3-app."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Japanese Reader</title>
-        <meta
-          name="description"
-          content="A mobile-focused Japanese reader web app built with React, TypeScript, Next.js, TailwindCSS, and Zustand. Initialized with create-t3-app."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center bg-black text-white">
-        <header className="flex h-12 w-full items-center justify-center">
-          <div className="w-full max-w-2xl">
-            <h1 className="px-4 text-lg font-bold">Japanese Reader</h1>
-          </div>
-        </header>
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col items-center bg-black text-white">
+      <header className="flex h-12 w-full items-center justify-center">
+        <div className="w-full max-w-2xl">
+          <h1 className="px-4 text-lg font-bold">Japanese Reader</h1>
+        </div>
+      </header>
+    </main>
   );
 }
