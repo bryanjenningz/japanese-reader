0cf74de20ee5789c670cf2b09ba7f8cbe2f2023e
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-03 05:18:39 -0700

    Make header position fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-black text-white">
-      <header className="flex h-12 w-full items-center justify-center">
+      <header className="fixed left-0 right-0 top-0 flex h-12 w-full items-center justify-center bg-inherit">
         <div className="flex h-full w-full max-w-2xl items-center justify-between">
           <h1 className="px-4 text-lg font-bold">Japanese Reader</h1>
 
@@ -48,7 +48,7 @@ export default function Home() {
         </div>
       </header>
 
-      <p className="w-full max-w-2xl px-4 pb-60 text-xl">
+      <p className="w-full max-w-2xl px-4 pb-60 pt-14 text-xl">
         {selectedEntry?.text.split("").map((char, i) => {
           return <span key={`${char}-${i}`}>{char}</span>;
         })}
