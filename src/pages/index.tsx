fc6bb1d1e46b30bcbb11c07caa72738bc9f83db9
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-03 03:38:25 -0700

    Add paste Japanese text button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import { PasteContentGoIcon } from "~/icons/PasteContentGoIcon";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-black text-white">
       <header className="flex h-12 w-full items-center justify-center">
-        <div className="w-full max-w-2xl">
+        <div className="flex h-full w-full max-w-2xl items-center justify-between">
           <h1 className="px-4 text-lg font-bold">Japanese Reader</h1>
+          <button
+            className="flex h-12 w-12 items-center justify-center"
+            title="Paste Japanese text"
+          >
+            <PasteContentGoIcon />
+          </button>
         </div>
       </header>
     </main>
