4bacfac3c6d7a07a01972301baadb45829ac5275
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-03 03:43:49 -0700

    Add history link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { HistoryIcon } from "~/icons/HistoryIcon";
 import { PasteContentGoIcon } from "~/icons/PasteContentGoIcon";
 
 export default function Home() {
@@ -6,12 +8,23 @@ export default function Home() {
       <header className="flex h-12 w-full items-center justify-center">
         <div className="flex h-full w-full max-w-2xl items-center justify-between">
           <h1 className="px-4 text-lg font-bold">Japanese Reader</h1>
-          <button
-            className="flex h-12 w-12 items-center justify-center"
-            title="Paste Japanese text"
-          >
-            <PasteContentGoIcon />
-          </button>
+
+          <div className="flex">
+            <button
+              className="flex h-12 w-12 items-center justify-center"
+              title="Paste Japanese text"
+            >
+              <PasteContentGoIcon />
+            </button>
+
+            <Link
+              className="flex h-12 w-12 items-center justify-center"
+              title="See history"
+              href="/history"
+            >
+              <HistoryIcon />
+            </Link>
+          </div>
         </div>
       </header>
     </main>
