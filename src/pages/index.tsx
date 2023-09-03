9e3a9dd662ff64cd67463d2cfe2ebbb9b17cb6cb
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-03 05:13:46 -0700

    Add history entry when Japanese text is pasted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { HistoryIcon } from "~/icons/HistoryIcon";
 import { PasteContentGoIcon } from "~/icons/PasteContentGoIcon";
+import { useHistoryAction, useHistoryState } from "~/stores/historyStore";
 
 export default function Home() {
+  const selectedEntry = useHistoryState((x) => x.selectedEntry);
+  const addEntry = useHistoryAction((x) => x.addEntry);
+
   return (
     <main className="flex min-h-screen flex-col items-center bg-black text-white">
       <header className="flex h-12 w-full items-center justify-center">
@@ -13,6 +17,22 @@ export default function Home() {
             <button
               className="flex h-12 w-12 items-center justify-center"
               title="Paste Japanese text"
+              onClick={() => {
+                void (async () => {
+                  try {
+                    addEntry({
+                      text: await navigator.clipboard.readText(),
+                      time: Date.now(),
+                    });
+                  } catch {
+                    const text = prompt("Paste Japanese text");
+                    if (text === null) {
+                      return;
+                    }
+                    addEntry({ text, time: Date.now() });
+                  }
+                })();
+              }}
             >
               <PasteContentGoIcon />
             </button>
@@ -27,6 +47,12 @@ export default function Home() {
           </div>
         </div>
       </header>
+
+      <p className="w-full max-w-2xl px-4 pb-60 text-xl">
+        {selectedEntry?.text.split("").map((char, i) => {
+          return <span key={`${char}-${i}`}>{char}</span>;
+        })}
+      </p>
     </main>
   );
 }
